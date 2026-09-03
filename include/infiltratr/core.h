e9b747c7c0530590fbba1f57788e046203ef8c4e
Shannon Smith <infiltratr@yandex.com>
2026-09-03 15:02:51 +1000

    Release 1.15.7: hosted rebuild refresh

--- include/infiltratr/core.h
+++ include/infiltratr/core.h
@@ -33,7 +33,7 @@
 extern "C" {
 #endif
 
-#define INFILTRATR_COMMON_VERSION "1.15.6"
+#define INFILTRATR_COMMON_VERSION "1.15.7"
 #define INFILTRATR_PROJECT_INFO_ABI 1U
 #define INFILTRATR_SCALE_OPTIONS_ABI 1U
 #define INFILTRATR_ARRAY_LENGTH(array) (sizeof(array) / sizeof((array)[0]))
